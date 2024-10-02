72d9df600f04303bce9c8105ad9db630bf394b9f
Mr.P <ssjblue197@gmail.com>
2024-10-02 16:36:53 +0700

    integrate with pureui

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { createApp } from 'vue'
-import './style.css'
+import 'pure-uikit/dist/themes/light.css';
+import 'pure-uikit/dist/themes/dark.css';
+import './assets/styles/index.scss';
 import App from './App.vue'
+
+import { setBasePath } from 'pure-uikit/dist/utilities/base-path.js';
+import { registerIconLibrary } from 'pure-uikit/dist/utilities/icon-library.js';
+import 'pure-uikit/dist/pure-ui.js';
+
+setBasePath('/public');
+registerIconLibrary('custom', {
+  resolver: (name: string) => `icons/${name}.svg`, //Public folder after bundle
+  mutator: (svg: SVGElement) => {
+    svg.setAttribute('fill', 'none');
+    svg.setAttribute('stroke', 'currentColor');
+  },
+});
 
 createApp(App).mount('#app').$nextTick(() => {
   // Use contextBridge
